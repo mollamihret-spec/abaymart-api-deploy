e5b80020fcfa14a0948357189a6dbcfa8c338046
mollamihret-spec <molamihert@gmail.com>
2026-02-09 10:58:30 +0300

    Update extractBudget.js

--- helpers/extractBudget.js
+++ helpers/extractBudget.js
@@ -1,12 +1,17 @@
 function extractBudget(question) {
   if (!question) return null;
 
-  // Match any number preceded optionally by $ or 'under' or 'less than'
-  const regex = /(?:under|less than|<=?)?\s*\$?(\d+(?:\.\d{1,2})?)/i;
+  // Match "under $50", "below $100", "less than 30", "$50", "50 USD"
+  const regex = /(?:under|below|less than)?\s*\$?\s*(\d+(?:\.\d{1,2})?)/i;
+
   const match = question.match(regex);
 
-  if (match) return parseFloat(match[1]);
-  return null;
+  if (match) {
+    const value = parseFloat(match[1]);
+    if (!isNaN(value)) return value;
+  }
+
+  return null; // no budget found
 }
 
 module.exports = { extractBudget };
